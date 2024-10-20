bffe8d3341bccc926aaa874fcde2cc87bbba0b62
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-10-19 23:50:27 -0300

    :sparkles: feat: added first access routes

--- src/shared/routes/auth.route.tsx
+++ src/shared/routes/auth.route.tsx
@@ -3,6 +3,8 @@ import { LoginPage } from '@/modules/auth/pages/login/login.page';
 import { Route, Routes } from 'react-router-dom';
 import { AuthLayout } from '@/modules/auth/pages/auth.layout';
 import { FirstAccessRegistryPage } from '@/modules/auth/pages/(first-access)/registry/first-access-registry.page';
+import { FirstAccessVerifyPage } from '@/modules/auth/pages/(first-access)/verify/first-access-verify.page';
+import { FirstAccessSuccessfullyPage } from '@/modules/auth/pages/(first-access)/successfully/first-access-successfully.page';
 
 export function AuthRoute() {
 	return (
@@ -10,7 +12,9 @@ export function AuthRoute() {
 			<Routes>
 				<Route element={<AuthLayout />}>
 					<Route path='/login' element={<LoginPage />} />
-					<Route path='first-access/registry' element={<FirstAccessRegistryPage />} />
+					<Route path='/first-access/registry' element={<FirstAccessRegistryPage />} />
+					<Route path='/first-access/verify' element={<FirstAccessVerifyPage />} />
+					<Route path='/first-access/successfully' element={<FirstAccessSuccessfullyPage />} />
 				</Route>
 			</Routes>
 		</AuthProvider>
