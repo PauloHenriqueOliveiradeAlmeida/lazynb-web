6567cf109a7bad430bfe01ccf83fc440bedfa144
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-10-21 22:33:14 -0300

    :sparkles: feat: added forgot password routes

--- src/shared/routes/auth.route.tsx
+++ src/shared/routes/auth.route.tsx
@@ -5,6 +5,10 @@ import { AuthLayout } from '@/modules/auth/pages/auth.layout';
 import { FirstAccessRegistryPage } from '@/modules/auth/pages/(first-access)/registry/first-access-registry.page';
 import { FirstAccessVerifyPage } from '@/modules/auth/pages/(first-access)/verify/first-access-verify.page';
 import { FirstAccessSuccessfullyPage } from '@/modules/auth/pages/(first-access)/successfully/first-access-successfully.page';
+import { ForgotPasswordSendPage } from '@/modules/auth/pages/(forgot-password)/send/forgot-password-send.page';
+import { ForgotPasswordVerifyPage } from '@/modules/auth/pages/(forgot-password)/verify/forgot-password-verify.page';
+import { ForgotPasswordRegistryPage } from '@/modules/auth/pages/(forgot-password)/registry/forgot-password-registry.page';
+import { ForgotPasswordSuccessfullyPage } from '@/modules/auth/pages/(forgot-password)/successfully/forgot-password-successfully.page';
 
 export function AuthRoute() {
 	return (
@@ -15,6 +19,10 @@ export function AuthRoute() {
 					<Route path='/first-access/registry' element={<FirstAccessRegistryPage />} />
 					<Route path='/first-access/verify' element={<FirstAccessVerifyPage />} />
 					<Route path='/first-access/successfully' element={<FirstAccessSuccessfullyPage />} />
+					<Route path='/forgot-password/send' element={<ForgotPasswordSendPage />} />
+					<Route path='/forgot-password/verify' element={<ForgotPasswordVerifyPage />} />
+					<Route path='/forgot-password/registry' element={<ForgotPasswordRegistryPage />} />
+					<Route path='/forgot-password/successfully' element={<ForgotPasswordSuccessfullyPage />} />
 				</Route>
 			</Routes>
 		</AuthProvider>
