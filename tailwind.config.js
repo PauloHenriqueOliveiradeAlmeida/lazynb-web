af1a85a243b78217b360279f4a0ec1f5e7f78618
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-10-24 23:10:54 -0300

    :sparkles: feat: added error color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{jsx,tsx}"],
+	content: ['./index.html', './src/**/*.{jsx,tsx}'],
 	theme: {
 		extend: {},
 		colors: {
@@ -8,9 +8,9 @@ export default {
 			primary: '#545F71',
 			secondary: '#121212',
 			white: '#f1f1f1',
-			dark: '#121212'
-		}
+			dark: '#121212',
+			error: '#d80032',
+		},
 	},
 	plugins: [],
-}
-
+};
