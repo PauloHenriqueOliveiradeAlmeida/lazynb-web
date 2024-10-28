516870022fb31c89d9ffb70d1231b9d1ddb5e00b
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-10-28 19:56:59 -0300

    :recycle: refactor: added formik field to otp input

--- src/shared/components/input-otp/input-otp.component.tsx
+++ src/shared/components/input-otp/input-otp.component.tsx
@@ -1,4 +1,3 @@
-import { useField } from 'formik';
 import { useInputOTP } from './input-otp.hook';
 
 interface InputOTPProps {
@@ -6,29 +5,28 @@ interface InputOTPProps {
 }
 
 export const InputOTP = ({ name }: InputOTPProps) => {
-	const [field, meta] = useField(name);
-	const { states, handlers } = useInputOTP(field.value, field.onChange);
+	const { states, handlers } = useInputOTP(name);
 
 	return (
 		<div className='flex gap-4 items-center justify-center'>
-			{states.otpNumbers.map((value, index) => {
-				return (
-					<>
-						<input
-							key={`otp-input-${index}`}
-							type='number'
-							maxLength={1}
-							{...field}
-							value={value}
-							onChange={(event) => handlers.handleChange(event.target.value, index)}
-							onKeyUp={(event) => handlers.handleKeyUp(event.key, index)}
-							ref={(input) => (states.inputRefs.current[index] = input as HTMLInputElement)}
-							className='w-12 text-primary text-center py-4 text-lg font-bold bg-white border-2 rounded-md border-primary outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
-						/>
-						{meta.touched && meta.error && <div className='text-red-500'>{meta.error}</div>}
-					</>
-				);
-			})}
+			{states.otpNumbers.map((value, index) => (
+				<>
+					<input
+						key={`otp-input-${index}`}
+						type='number'
+						maxLength={1}
+						max={1}
+						{...states.field}
+						value={value}
+						onChange={(event) => handlers.handleChange(event.target.value, index)}
+						onBlur={(event) => handlers.handleBlur(event.target.value, index)}
+						onKeyUp={(event) => handlers.handleKeyUp(event.key, index)}
+						ref={(input) => (states.inputRefs.current[index] = input as HTMLInputElement)}
+						className='w-12 text-primary text-center py-4 text-lg font-bold bg-white border-2 rounded-md border-primary outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
+					/>
+					{states.meta.touched && states.meta.error && <div className='text-red-500'>{states.meta.error}</div>}
+				</>
+			))}
 		</div>
 	);
 };
--- src/shared/components/input-otp/input-otp.hook.ts
+++ src/shared/components/input-otp/input-otp.hook.ts
@@ -1,14 +1,26 @@
+import { useField } from 'formik';
 import { useRef } from 'react';
 
-export function useInputOTP(value: string, onChange: (value: string) => void) {
+export function useInputOTP(name: string) {
 	const inputRefs = useRef<HTMLInputElement[]>([]);
-	const otpNumbers = value.split('').concat(Array(6 - value.length).fill(''));
+	const [field, meta] = useField(name);
+	const otpNumbers: string[] = field.value.split('').concat(Array(6 - field.value.length).fill(''));
 
 	const handleChange = (value: string, index: number) => {
+		const newOtpValues = generateNewOtpValues(value, index);
+		field.onChange({ target: { name, value: newOtpValues.join('') } });
+	};
+
+	const handleBlur = (value: string, index: number) => {
+		const newOtpValues = generateNewOtpValues(value, index);
+		field.onBlur({ target: { name, value: newOtpValues.join('') } });
+	};
+
+	const generateNewOtpValues = (value: string, index: number) => {
 		const [sanitizedValue] = value.replace(/[\D]/g, '');
 		const newOtpValues = [...otpNumbers];
 		newOtpValues[index] = sanitizedValue || '';
-		onChange(newOtpValues.join(''));
+		return newOtpValues;
 	};
 
 	const handleKeyUp = (key: string, index: number) => {
@@ -23,7 +35,7 @@ export function useInputOTP(value: string, onChange: (value: string) => void) {
 	};
 
 	return {
-		states: { otpNumbers, inputRefs },
-		handlers: { handleChange, handleKeyUp },
+		states: { otpNumbers, inputRefs, field, meta },
+		handlers: { handleChange, handleKeyUp, handleBlur },
 	};
 }
