93f3dc6a0cf9eb3c1eb2dda51f58f0da99a2a747
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-11-03 22:32:36 -0300

    :sparkles: feat: added authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Router } from './shared/routes/setup';
-
+import 'react-toastify/dist/ReactToastify.css';
+import { BaseLayout } from './shared/layouts/base/base.layout';
+import { BaseProvider } from './shared/layouts/base/base.context';
+import { AuthenticationProvider } from './shared/contexts/authentication/authentication.context';
 export function App() {
 	return (
-		<>
-			<Router />
-		</>
+		<AuthenticationProvider>
+			<BaseProvider>
+				<BaseLayout />
+			</BaseProvider>
+		</AuthenticationProvider>
 	);
 }
