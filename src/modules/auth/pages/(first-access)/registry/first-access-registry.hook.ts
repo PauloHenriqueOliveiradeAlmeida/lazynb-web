5138573edda40da9c81948fe4f174d93ff3049ee
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-11-17 17:04:03 -0300

    :sparkles: feat: first access registry integration

---
+++ src/modules/auth/pages/(first-access)/registry/first-access-registry.hook.ts
@@ -0,0 +1,35 @@
+import { FirstAccessRegistrySchema } from './first-access-registry.schema';
+import { useNavigate } from 'react-router-dom';
+import { useFirstAccessContext } from '../first-access.hook';
+import { useHttp } from '@/shared/services/http/http.hook';
+import { AuthService } from '@/shared/services/http/auth/auth.service';
+import { toast } from 'react-toastify';
+
+type firstAccessRegistrychemaValues = typeof FirstAccessRegistrySchema._type;
+
+export function useFirstAccessRegistryPage() {
+	const {
+		handlers: { setEmail, setPassword, setConfirmPassword },
+	} = useFirstAccessContext();
+	const {
+		handlers: { request },
+	} = useHttp();
+	const navigate = useNavigate();
+
+	const authService = new AuthService();
+
+	const handleSubmit = async (values: firstAccessRegistrychemaValues) => {
+		const response = await request(() => authService.sendFirstAccessEmail(values.email));
+		if (!response) return;
+
+		toast.success(response.message);
+		setEmail(values.email);
+		setPassword(values.password);
+		setConfirmPassword(values.confirmPassword);
+		navigate('/first-access/verify');
+	};
+
+	return {
+		handlers: { handleSubmit },
+	};
+}
--- src/modules/auth/pages/(first-access)/registry/first-access-registry.page.tsx
+++ src/modules/auth/pages/(first-access)/registry/first-access-registry.page.tsx
@@ -2,31 +2,35 @@ import { Input } from '@/shared/components/input/input.component';
 import { InputPassword } from '@/shared/components/input-password/input-password.component';
 import { useAuthContext } from '@/modules/auth/providers/contexts/auth.hook';
 import { Button } from '@/shared/components/button/button.component';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Formik, Form } from 'formik';
 import { toFormikValidationSchema } from 'zod-formik-adapter';
 import { FirstAccessRegistrySchema } from './first-access-registry.schema';
+import { useFirstAccessRegistryPage } from './first-access-registry.hook';
 
 export function FirstAccessRegistryPage() {
 	const {
 		handlers: { setAuthPageTitle },
 	} = useAuthContext();
 	setAuthPageTitle('Primeiro Acesso');
-	const navigate = useNavigate();
+
+	const {
+		handlers: { handleSubmit },
+	} = useFirstAccessRegistryPage();
 
 	return (
 		<main className='w-96'>
 			<Formik
-				initialValues={{}}
+				initialValues={{ email: '', password: '', confirmPassword: '' }}
 				validationSchema={toFormikValidationSchema(FirstAccessRegistrySchema)}
-				onSubmit={() => {}}
+				onSubmit={handleSubmit}
 			>
 				<Form className='flex flex-col gap-16'>
 					<Input name='email' placeholder='Email' />
 					<InputPassword name='password' placeholder='Senha' />
 					<InputPassword name='confirmPassword' placeholder='Confirmar a Senha' />
 					<div className='flex justify-around items-center'>
-						<Button label='Continuar' variant='filled' fullWidth onClick={() => navigate('/first-access/verify')} />
+						<Button label='Continuar' variant='filled' fullWidth />
 					</div>
 				</Form>
 			</Formik>
