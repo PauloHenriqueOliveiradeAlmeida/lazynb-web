b6616b9d7040d743f4b7643430c96b994b5165fa
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-11-17 17:04:58 -0300

    :sparkles: feat: forgot password send email integration

--- src/modules/auth/pages/(forgot-password)/send/forgot-password-send.page.tsx
+++ src/modules/auth/pages/(forgot-password)/send/forgot-password-send.page.tsx
@@ -5,6 +5,7 @@ import { Formik, Form } from 'formik';
 import { Link } from 'react-router-dom';
 import { toFormikValidationSchema } from 'zod-formik-adapter';
 import { ForgotPasswordSendSchema } from './forgot-password-send.schema';
+import { useForgotPasswordSendPage } from './forgot-password-send.hook';
 
 export function ForgotPasswordSendPage() {
 	const {
@@ -12,15 +13,19 @@ export function ForgotPasswordSendPage() {
 	} = useAuthContext();
 	setAuthPageTitle('Esqueceu a senha');
 
+	const {
+		handlers: { handleSubmit },
+	} = useForgotPasswordSendPage();
+
 	return (
 		<main className='w-2/4 place-items-center'>
 			<h3 className='mb-24 text-xl'>
 				Será enviado um código para redefinição da senha informe seu e-mail no campo abaixo.
 			</h3>
 			<Formik
-				initialValues={{}}
+				initialValues={{ email: '' }}
 				validationSchema={toFormikValidationSchema(ForgotPasswordSendSchema)}
-				onSubmit={() => {}}
+				onSubmit={handleSubmit}
 			>
 				<Form className='flex flex-col gap-16 w-96'>
 					<Input name='email' placeholder='Email' />
