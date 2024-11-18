3dd429d8b7425a207a0e6dffe1587688824a93ea
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-11-17 23:42:22 -0300

    :sparkles: feat: added className extension to button link

--- src/shared/components/button-link/button-link.component.tsx
+++ src/shared/components/button-link/button-link.component.tsx
@@ -5,19 +5,22 @@ interface ButtonLinkProps {
 	label: string;
 	variant: 'filled' | 'outlined';
 	fullWidth?: boolean;
+	className?: string;
 }
 
-export const ButtonLink = ({ to, label, variant, fullWidth }: ButtonLinkProps) => (
+export const ButtonLink = ({ to, label, variant, fullWidth, className }: ButtonLinkProps) => (
 	<Link
 		className={`px-10 py-2
 			border-primary border-2
 			rounded-md transition-all
+			text-center
 			${variant === 'filled' ? 'text-dark' : 'text-primary'}
 			${variant === 'filled' ? 'bg-primary' : 'bg-transparent'}
 			${variant === 'filled' ? 'hover:bg-transparent' : 'hover:bg-primary'}
 			${variant === 'filled' ? 'text-white' : 'text-primary'}
 			${variant === 'filled' ? 'hover:text-primary' : 'hover:text-white'}
-			${fullWidth && 'w-full'}`}
+			${fullWidth && 'w-full'}
+			${className}`}
 		to={to}
 	>
 		{label}
