f4221af5cf72d6285ad41d6f011f5df67386eda3
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-11-19 22:46:26 -0300

    :sparkles: feat: colab routes

--- src/shared/routes/app.route.tsx
+++ src/shared/routes/app.route.tsx
@@ -1,12 +1,34 @@
-import { AuthProvider } from '@/modules/auth/providers/contexts/auth.context';
+import { ClientReportPage } from '@/modules/app/(client)/report/client-report.page';
 import { Route, Routes } from 'react-router-dom';
+import { FormProvider } from '../layouts/form/form.context';
+import { FormLayout } from '../layouts/form/form.layout';
+import { ClientNewPage } from '@/modules/app/(client)/new/client-new.page';
+import { ClientEditPage } from '@/modules/app/(client)/edit/client-edit.page';
+import { CollaboratorReportPage } from '@/modules/app/(collaborator)/report/collaborator-report.page';
+import { CollaboratorNewPage } from '@/modules/app/(collaborator)/new/collaborator-new.page';
+import { CollaboratorEditPage } from '@/modules/app/(collaborator)/edit/collaborator-edit.page';
 
 export function AppRoute() {
 	return (
-		<AuthProvider>
-			<Routes>
-				<Route path='*' element={<>app</>} />
-			</Routes>
-		</AuthProvider>
+		<Routes>
+			<Route path='/client/report' element={<ClientReportPage />} />
+			<Route path='/collaborator/report' element={<CollaboratorReportPage />} />
+
+			<Route
+				path='*'
+				element={
+					<FormProvider>
+						<Routes>
+							<Route element={<FormLayout />}>
+								<Route path='/client/new' element={<ClientNewPage />} />
+								<Route path='/client/:id/edit' element={<ClientEditPage />} />
+								<Route path='/collaborator/new' element={<CollaboratorNewPage />} />
+								<Route path='/collaborator/:id/edit' element={<CollaboratorEditPage />} />
+							</Route>
+						</Routes>
+					</FormProvider>
+				}
+			/>
+		</Routes>
 	);
 }
--- src/shared/routes/auth.route.tsx
+++ src/shared/routes/auth.route.tsx
@@ -1,22 +1,22 @@
-import { AuthProvider } from '@/modules/auth/providers/contexts/auth.context';
-import { LoginPage } from '@/modules/auth/pages/login/login.page';
 import { Route, Routes } from 'react-router-dom';
-import { AuthLayout } from '@/modules/auth/pages/auth.layout';
-import { FirstAccessRegistryPage } from '@/modules/auth/pages/(first-access)/registry/first-access-registry.page';
-import { FirstAccessVerifyPage } from '@/modules/auth/pages/(first-access)/verify/first-access-verify.page';
-import { FirstAccessSuccessfullyPage } from '@/modules/auth/pages/(first-access)/successfully/first-access-successfully.page';
-import { ForgotPasswordSendPage } from '@/modules/auth/pages/(forgot-password)/send/forgot-password-send.page';
-import { ForgotPasswordVerifyPage } from '@/modules/auth/pages/(forgot-password)/verify/forgot-password-verify.page';
-import { ForgotPasswordRegistryPage } from '@/modules/auth/pages/(forgot-password)/registry/forgot-password-registry.page';
-import { ForgotPasswordSuccessfullyPage } from '@/modules/auth/pages/(forgot-password)/successfully/forgot-password-successfully.page';
-import { FirstAccessProvider } from '@/modules/auth/pages/(first-access)/first-access.context';
-import { ForgotPasswordProvider } from '@/modules/auth/pages/(forgot-password)/forgot-password.context';
+import { FormProvider } from '../layouts/form/form.context';
+import { FormLayout } from '../layouts/form/form.layout';
+import { LoginPage } from '@/modules/auth/login/login.page';
+import { FirstAccessProvider } from '@/modules/auth/(first-access)/first-access.context';
+import { FirstAccessRegistryPage } from '@/modules/auth/(first-access)/registry/first-access-registry.page';
+import { FirstAccessVerifyPage } from '@/modules/auth/(first-access)/verify/first-access-verify.page';
+import { FirstAccessSuccessfullyPage } from '@/modules/auth/(first-access)/successfully/first-access-successfully.page';
+import { ForgotPasswordProvider } from '@/modules/auth/(forgot-password)/forgot-password.context';
+import { ForgotPasswordSendPage } from '@/modules/auth/(forgot-password)/send/forgot-password-send.page';
+import { ForgotPasswordVerifyPage } from '@/modules/auth/(forgot-password)/verify/forgot-password-verify.page';
+import { ForgotPasswordRegistryPage } from '@/modules/auth/(forgot-password)/registry/forgot-password-registry.page';
+import { ForgotPasswordSuccessfullyPage } from '@/modules/auth/(forgot-password)/successfully/forgot-password-successfully.page';
 
 export function AuthRoute() {
 	return (
-		<AuthProvider>
+		<FormProvider>
 			<Routes>
-				<Route element={<AuthLayout />}>
+				<Route element={<FormLayout />}>
 					<Route path='/login' element={<LoginPage />} />
 					<Route
 						path='/first-access/*'
@@ -45,6 +45,6 @@ export function AuthRoute() {
 					/>
 				</Route>
 			</Routes>
-		</AuthProvider>
+		</FormProvider>
 	);
 }
