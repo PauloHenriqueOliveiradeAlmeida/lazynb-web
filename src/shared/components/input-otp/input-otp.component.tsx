29aaf1ebf8cc4fd1ddff71d757ba986cc2598d72
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-12-02 22:36:19 -0300

    :sparkles: feat: added logo

--- src/shared/components/input-otp/input-otp.component.tsx
+++ src/shared/components/input-otp/input-otp.component.tsx
@@ -7,6 +7,8 @@ interface InputOTPProps {
 export const InputOTP = ({ name }: InputOTPProps) => {
 	const { states, handlers } = useInputOTP(name);
 
+	const textColor = states.meta.touched && states.meta.error ? 'text-error' : 'text-primary';
+
 	return (
 		<div className='flex flex-col items-center justify-center'>
 			<div className='flex gap-4 items-center justify-center'>
@@ -22,7 +24,7 @@ export const InputOTP = ({ name }: InputOTPProps) => {
 							onBlur={(event) => handlers.handleBlur(event.target.value, index)}
 							onKeyUp={(event) => handlers.handleKeyUp(event.key, index)}
 							ref={(input) => (states.inputRefs.current[index] = input as HTMLInputElement)}
-							className='w-12 text-primary text-center py-4 text-lg font-bold bg-white border-2 rounded-md border-primary outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none'
+							className={`w-12 ${textColor} text-center py-4 text-lg font-bold bg-white border-2 rounded-md ${states.meta.touched && states.meta.error ? 'border-error' : 'border-primary'} outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none`}
 						/>
 					</>
 				))}
--- src/shared/layouts/form/form.layout.tsx
+++ src/shared/layouts/form/form.layout.tsx
@@ -1,4 +1,5 @@
-import backgroundImage from '@/shared/assets/images/form/background.image.png';
+import BackgroundImage from '@/shared/assets/images/form/background.image.png';
+import DarkLogoImage from '@/shared/assets/images/dark-logo.image.svg';
 import { Outlet } from 'react-router-dom';
 import { useFormContext } from './form.hook';
 
@@ -9,12 +10,13 @@ export function FormLayout() {
 	return (
 		<main className='flex w-full min-h-screen justify-between'>
 			<section
-				style={{ backgroundImage: `linear-gradient(#ffffff60, #ffffff60), url(${backgroundImage})` }}
+				style={{ backgroundImage: `linear-gradient(#ffffff60, #ffffff60), url(${BackgroundImage})` }}
 				className='flex flex-1 justify-center items-center bg-center bg-no-repeat bg-cover'
 			>
 				<h3 className='text-4xl font-bold text-primary'>{formPageTitle}</h3>
 			</section>
 			<section className='flex flex-col flex-1 justify-center items-center bg-white'>
+				<img src={DarkLogoImage} alt='Logo' className='w-20 mb-8' />
 				<Outlet />
 			</section>
 		</main>
